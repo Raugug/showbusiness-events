f7379ddc45a63507353aefa1b5aafc24d25fd6cf
RaulG <guiaraul@gmail.com>
2018-10-18 17:46:10 +0200

    Esta vez si que si

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e67012ce857a804cffd2f81943410b2.js"
+  "/precache-manifest.140e8267ce075eb2a993cdd49941945f.js"
 );
 
 workbox.clientsClaim();
