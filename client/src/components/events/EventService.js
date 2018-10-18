40e19868bfc71ffc4b411e1dc690f34d9f5b10ac
RaulG <guiaraul@gmail.com>
2018-10-18 14:20:32 +0200

    pre build

--- client/src/components/events/EventService.js
+++ client/src/components/events/EventService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 class _EventService {
   constructor() {
     this.service = axios.create({
-      baseURL: 'http://localhost:3001/api',
+      baseURL: process.env.REACT_APP_API_URL,
       withCredentials: true
     });
   }
